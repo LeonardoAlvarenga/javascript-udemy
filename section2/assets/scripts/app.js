7c07ac8eede0594efece54a7114def4f24ce6886
Leonardo Alvarenga <leonardo.mad@hotmail.com>
2021-04-25 16:27:46 -0300

    Monster Killer Project - 83

--- section2/assets/scripts/app.js
+++ section2/assets/scripts/app.js
@@ -31,6 +31,16 @@ function writeToLog(
 
 function calculateResult(calculationType) {
   const enteredNumber = getUserNumberInput();
+  if (
+    (calculationType !== 'ADD' &&
+      calculationType !== 'SUBTRACT' &&
+      calculationType !== 'MULTIPLY' &&
+      calculationType !== 'DIVIDE') ||
+    !enteredNumber
+  ) {
+    return;
+  }
+
   const initialResult = currentResult;
   let mathOperator;
 
