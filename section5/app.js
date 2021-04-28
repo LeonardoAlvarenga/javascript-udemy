566765da77534bad2a414d84fe5ef348a40326f0
Leonardo Alvarenga <leonardo.mad@hotmail.com>
2021-04-28 16:43:54 -0300

    Till End of Section 5 - 122

---
+++ section5/app.js
@@ -0,0 +1,19 @@
+// let name = 'Lele';
+
+// if (name === 'Lele') {
+//   let hobbies = ['brincar', 'cantar', 'dançar'];
+//   console.log(hobbies);
+// }
+
+// function greet() {
+//   let age = 30;
+//   let name = 'Manuel';
+//   console.log(name, age, hobbies);
+// }
+
+// console.log(name);
+'use strict';
+
+const name = 'Lele';
+
+console.log(name);
